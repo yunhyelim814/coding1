56caad508aae131cc0abc97d9e99e3281a89b785
yunhyelim814 <hyelimyun623@gmail.com>
2024-09-11 16:24:58 +0900

    Update test1.py

--- views/test1.py
+++ views/test1.py
@@ -18,7 +18,7 @@ st.divider()  # 가로 긴 줄 구분선
 st.markdown(
     '''
     This is main text.
-    This is how to change the color of text :#F15F5F[색상,] :blue[Blue,] :green[Green.]
+    This is how to change the color of text :red[Red,] :blue[Blue,] :green[Green.]
     This is **Bold** and *Italic* text
     '''
 )
