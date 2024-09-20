a3104b2faad93ba3b8a890e5f427a0d2a67173fe
yunhyelim814 <hyelimyun623@gmail.com>
2024-09-20 16:39:25 +0900

    Update test3.py

--- views/test3.py
+++ views/test3.py
@@ -1,4 +1,27 @@
 import streamlit as st
+import random 
 
-st.title("스트림릿 테스트 3")
+# 게임 결과 내는 함수, guessNum는 사람 추측값 0(앞면), 1(뒷면)
+def playGame(guessNum) :
+  comNum = random.randint(0, 1)
+  if comNum == guessNum :
+    st.write('적중!')
+  else:
+    st.write('아쉽네요. 틀렸습니다.')
+  
+
+st.title("동전 던지기 게임")
+st.divider()
+
+st.image('assets/coin_head.png')
+st.image('assets/coin_tail.png')
+
+st.header("동전 던지기 게임에 오신 것을 환영합니다.")
+st.subheader('앞면일까요? 뒷면일까요?')
+
+if st.button('앞면'):
+  playGame(0)
+
+if st.button('뒷면') :
+  playGame(1)
 
